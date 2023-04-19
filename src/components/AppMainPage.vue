b21cf72640fa902231d89be48c04e93fc4601f97
silviosalza <silviofilipposalza@gmail.com>
2023-04-19 18:41:29 +0200

    sass + hovered (not functional)

--- src/components/AppMainPage.vue
+++ src/components/AppMainPage.vue
@@ -5,15 +5,12 @@ export default{
     data (){
         return{
             store,
+            isHovered: true
         }
     },
     methods:{
         languageFlag(){
             
-            if (this.store.movies.original_language === "en"){
-                console.log(this.store.movies.original_language )
-                
-            }
         }
     }
   
@@ -26,57 +23,60 @@ export default{
         <div class="movies">
             <h2>Movies</h2>
             <ul class="d-flex flex-wrap">
-                <li class="col-1" v-for="(movie,index) in store.movies" :key="index">
-                    <img :src="`${store.imgURL}${movie.poster_path}`" alt="">
-                    <ul>
-                        <li>{{ movie.title }}</li>
-                        <li>{{ movie.original_title }}</li>
-                        <li> {{ movie.original_language }} <img class="languageIcon" src="../assets/Akrotiri-32.png" alt=""></li>
-                        <li>{{ movie.vote_average }}</li>
-                    </ul>
+                <li class="col-2" v-for="(movie,index) in store.movies" :key="index">
+                    <div class="card" @mouseover="isHovered = false" @mouseleave="isHovered = true">
+                        <img class="frontimg" v-show="isHovered" :src="`${store.imgURL}${movie.poster_path}`" alt="">
+                        <ul v-show="!isHovered">
+                            <li>{{ movie.title }}</li>
+                            <li>{{ movie.original_title }}</li>
+                            <li> {{ movie.original_language }} <img class="languageIcon" src="../assets/Akrotiri-32.png" alt=""></li>
+                            <li>{{ movie.vote_average }}</li>
+                        </ul>
+                    </div>
+
                 </li>
             </ul>
         </div>
         <div class="tvseries">
             <h2>TV Series</h2>
             <ul class="d-flex flex-wrap">
-                <li class="col-1" v-for="(tvshow,index) in store.TVshows" :key="index">
-                    <img :src="`${store.imgURL}${tvshow.poster_path}`" alt="">
-                    <ul>
-                        <li>{{ tvshow.name }}</li>
-                        <li>{{ tvshow.original_name }}</li>
-                        <li>{{ tvshow.original_language }}</li>
-                        <li>{{ tvshow.vote_average }}</li>
-                    </ul>
+                <li class="col-2" v-for="(tvshow,index) in store.TVshows" :key="index">
+                    <div class="card" @mouseover="isHovered = false" @mouseleave="isHovered = true">
+                        <img v-show="isHovered" class="frontimg" :src="`${store.imgURL}${tvshow.poster_path}`" alt="">
+                        <ul v-show="!isHovered">
+                            <li>{{ tvshow.name }}</li>
+                            <li>{{ tvshow.original_name }}</li>
+                            <li>{{ tvshow.original_language }}</li>
+                            <li>{{ tvshow.vote_average }}</li>
+                        </ul>
+                    </div>
                 </li>
             </ul>
         </div>
-
-
-
     </div>
 </template>
 
 <style scoped lang="scss">
+
 .main{
-    height:calc(100vh - 100px);
-    background-color: rgb(0, 0, 0, 0.3);
+    background-color: rgb(0, 0, 0, 0.3); 
+    .card{
+        height: 342px;
+        margin: 10px;
+        border-radius: 10px;
+        .frontimg{
+            height: 342px;
+            border-radius: 10px;
+        }
+    }
  }
  ul{
     padding: 0;
  }
- 
  li{
     list-style: none;
- }
- img{
-    width: 100px;
  }
  .languageIcon{
     width: 20px;
  }
- 
- 
-
-
 </style>
