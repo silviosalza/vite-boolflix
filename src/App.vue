11854e0dfd96a89764bb2374fab628588330d45d
silviosalza <silviofilipposalza@gmail.com>
2023-04-19 11:44:14 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script >
+import axios from "axios";
+import {store} from "./store";
+
+export default{
+  components: {
+    
+  },
+  data(){
+    return{
+      store
+    }
+
+  }
+}
+
+
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss" >
+
+</style>
---
+++ src/store.js
@@ -0,0 +1,11 @@
+import {reactive} from "vue";
+
+export const store = reactive({
+ 
+
+    movies: [],
+    TVshows: [],
+    
+
+
+})
